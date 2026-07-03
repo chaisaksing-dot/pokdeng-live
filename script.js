c99a76e8f844c9e37fc4999aca5d9d90670f9832
chaisaksing-dot <chaisaksing@gmail.com>
2026-07-03 19:47:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -420,8 +420,8 @@ function listenRoom(roomId) {
     if (el("minBetText")) el("minBetText").innerText = room.minBet || 10;
     if (el("maxBetText")) el("maxBetText").innerText = room.maxBet || 0;
     if (el("tongPercentText")) {
-       (l("tongPercentText").innerText = room.tongPercent ?? 0 + "%";
-    }
+  el("tongPercentText").innerText = (room.tongPercent || 0) + "%";
+}
 
     renderPlayers();
     renderBetBox();
