1dde4f816561d178f397a472c5e0c50316608c3c
chaisaksing-dot <chaisak_sing@icloud.com>
2026-07-06 12:21:55 +0700

    fix liff id global

---
+++ js/confix.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2huyYMc8TD0oA7SJ1sfaejgpcb2H7x0U",
+  authDomain: "kang-card-game.firebaseapp.com",
+  databaseURL: "https://kang-card-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kang-card-game",
+  storageBucket: "kang-card-game.firebasestorage.app",
+  messagingSenderId: "400713700794",
+  appId: "1:400713700794:web:726cb6e525026a90a53983"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+window.LIFF_ID = "2010387758-ZiMGYm5E";
